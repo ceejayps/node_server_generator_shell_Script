8d7d71cbdfbc451989c502599293dd479cc04ce9
ceejayps <ceejayps1308@gmail.com>
2023-02-15 08:00:12 -0500

    added auth, roleshema and userschema

--- project-init.sh
+++ project-init.sh
@@ -7,7 +7,7 @@ read project_name
 echo "Creating a new directory for the project..."
 
 # Create a new directory for the project
-cd ..
+#cd ..
 mkdir $project_name
 
 # Navigate to the new directory
@@ -19,7 +19,7 @@ echo "Initializing the Node.js project..."
 clear
 npm init -y
 
-mkdir -p src/{server/{database,models,routes,middleware},pages,views,assets,services}
+mkdir -p src/{server/{database/schemas,models,routes,middleware},pages,views,assets,services}
 clear
 echo "Choose a database: "
 echo "1) MongoDB"
@@ -48,7 +48,153 @@ const db = async () => {
 
 export default db;" > connection.js
 
-cd ../../..
+#create user schema
+
+echo "import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Role',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export { User };
+
+"> schemas/user.js
+
+echo "import mongoose from 'mongoose';
+
+const roleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    enum: ['public', 'authenticated', 'admin', 'superadmin'],
+    required: true,
+    unique: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Role = mongoose.model('Role', roleSchema);
+
+export { Role };
+"> schemas/roles.js
+
+
+cd ..
+
+echo "Setting up Auth..."
+
+echo "
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { User } from './userSchema.js';
+import { Role } from './roleSchema.js';
+
+// Register a new user
+async function registerUser(req, res) {
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({
+      \$or: [{ email: req.body.email }, { username: req.body.username }]
+    });
+    if (existingUser) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    // Hash the password using bcrypt
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Find the role with the name 'public' and assign it to the user
+    const role = await Role.findOne({ name: 'public' });
+    if (!role) {
+      return res.status(500).json({ message: 'Role not found' });
+    }
+
+    // Create a new user and save it to the database
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      role: role._id // Assign the role's ID to the user
+    });
+    const savedUser = await newUser.save();
+
+    // Create a JWT token for the new user
+    const token = jwt.sign({ userId: savedUser._id }, 'mysecretkey');
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+const loginUser = async (req, res) => {
+  const { identifier, password } = req.body;
+
+  try {
+    const user = await User.findOne({
+      \$or: [{ username: identifier }, { email: identifier }],
+    }).populate('role');
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET);
+
+    res.json({ message: 'Login successful', accessToken, user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+
+export { registerUser, loginUser };
+"> controllers/auth.js
+
 
 # Prompt the user for the MongoDB connection information
 clear
@@ -79,11 +225,12 @@ echo "LIVE_DATABASE_URL=$uri" >> .env
 echo "MongoDB URI saved to .env as LIVE_DATABASE_URL"
 
 
+
 elif [ $choice -eq 2 ]; then
   db="postgresql"
   echo "Setting up PostgreSQL..."
   npm install pg
-  cd src/server/database 
+  cd src/server
 
   echo "import { Client } from 'pg';
 
@@ -143,7 +290,6 @@ else
   exit 1
 fi
 
-echo "You selected $db"
 
 echo "Installing dependencies..."
 
@@ -174,6 +320,10 @@ import bcrypt from 'bcrypt';
 
 // Importing database connection file
 import db from './src/server/database/connection.js'
+
+// Importing schemas connection file
+import { User } from './src/server/database/schemas/user.js';
+import { Role } from './src/server/database/schemas/role.js';
 
 // Initializing Express server
 const server = express();
